867d98a106b2414168266fe0c7944f45c0c27749
GustavoAlvesDeOliveira <gustavo.aoliveira@sptech.school>
2025-05-20 14:03:39 -0300

    app.js configurado

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
+var salasRouter = require("./src/routes/salas");
 var empresasRouter = require("./src/routes/empresas");
+var sensoresRouter = require("./src/routes/sensores");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,8 +33,9 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
+app.use("/salas", salasRouter);
 app.use("/empresas", empresasRouter);
+app.use("/sensores", sensoresRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
