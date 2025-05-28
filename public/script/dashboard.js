d21009a359240157ddfc4181a8cabaa753722301
IsaakGuilherme <isaak.guilherme@sptech.school>
2025-05-28 13:56:05 -0300

    Adição da função da listagem dos datacenters e criação dos card (Incompleta - Necessita de alterações no css)

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -1,5 +1,15 @@
 function listarDatacenters() {
-    const datacenters = sessionStorage.DATACENTERS;
-    
-    
+    cardDatacenter = document.getElementById("lista_status")
+    JSON.parse(sessionStorage.DATACENTERS).forEach(item => {
+        console.log('AAAAAAA')
+        console.log(item.datacenter)
+            cardDatacenter.innerHTML += `
+            <div class="painel-status-datacenter">
+                <div class="lista-status" id="lista_status">
+                    <a href="PainelGeral.html" class="salas"><div class="status alerta"></div><span id="nomeDatacenter">${item.datacenter}</span></a>
+                </div>
+            </div>
+            `
+
+    });
 }
