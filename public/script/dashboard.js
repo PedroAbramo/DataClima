215ae08cb9c9072fa5836a18b284aa6e61c4d502
rafael <rafael.vieira@sptech.school>
2025-06-06 19:37:32 -0300

    ajuste painel de datacenters

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -1,18 +1,12 @@
 function listarDatacenters() {
-    cardDatacenter = document.getElementById("lista_status")
+    cardDatacenter = document.getElementById("painel-datacenter")
     JSON.parse(sessionStorage.DATACENTERS).forEach(item => {
         console.log('AAAAAAA')
         console.log(item.datacenter)
             cardDatacenter.innerHTML += `
-<<<<<<< HEAD
-                    <a href="PainelGeral.html" class="salas" ><div class="status alerta"></div><span id="nomeDatacenter">${item.datacenter}</span></a>
-=======
-            <div class="painel-status-datacenter">
                 <div class="lista-status" id="lista_status">
                     <a href="PainelGeral2.html" class="salas" onclick="selecionarDatacenter(${item.id})"><div class="status alerta"></div><span id="nomeDatacenter">${item.datacenter}</span></a>
                 </div>
-            </div>
->>>>>>> a26fe66978025c2ef99513d573119bc6cf50c7f2
             `
     });
 }
