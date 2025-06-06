d34acfdb129e6130fd8d2b183ed755954aacdf97
rafael <rafael.vieira@sptech.school>
2025-06-06 19:12:04 -0300

    ajuste na implementação de datacenters

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -4,11 +4,7 @@ function listarDatacenters() {
         console.log('AAAAAAA')
         console.log(item.datacenter)
             cardDatacenter.innerHTML += `
-            <div class="painel-status-datacenter">
-                <div class="lista-status" id="lista_status">
-                    <a href="PainelGeral.html" class="salas" onclick="selecionarDatacenter(${item.id})"><div class="status alerta"></div><span id="nomeDatacenter">${item.datacenter}</span></a>
-                </div>
-            </div>
+                    <a href="PainelGeral.html" class="salas" ><div class="status alerta"></div><span id="nomeDatacenter">${item.datacenter}</span></a>
             `
     });
 }
