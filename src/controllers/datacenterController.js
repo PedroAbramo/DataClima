3c2020f06e6d9d545cfe337b0874ab4f5626ee60
PedroAbramo <pedro.dalmeida@sptech.school>
2025-06-07 20:31:34 -0300

    recebendo json para criar os gráficos (10 valores)

--- src/controllers/datacenterController.js
+++ src/controllers/datacenterController.js
@@ -15,6 +15,18 @@ function buscarDatacenterPorEmpresa(req, res) {
     res.status(500).json(erro.sqlMessage);
   });
 }
+
+function buscarRegistrosSala(req, res) {
+  var idSala = req.params.salaId;
+
+  salaModel.buscarRegistrosSala(idSala).then((resultado) => {
+    if (resultado.length > 0) {
+      res.status(200).json(resultado);
+    } 
+  })
+}
+
 module.exports = {
-  buscarDatacenterPorEmpresa
+  buscarDatacenterPorEmpresa,
+  buscarRegistrosSala
 }
