e966e08d87e041c56d0eb24474e0bc83285c2820
rafaeltokko <rafael.tokko@sptech.school>
2025-06-09 09:44:28 -0300

    mudança de nome

--- Documentos/ApiDat-aqu-ino/main.js
+++ Documentos/ApiDat-aqu-ino/main.js
@@ -53,22 +53,22 @@ const serial = async () => {
 
         if (temperatura > 32) {
             motivo.push("Temperatura muito alta");
-            tipo = "Critico"
+            tipo = "Crítico"
         } else if (temperatura < 18) {
             motivo.push("Temperatura baixa");
-            tipo = tipo === "Critico" ? "Critico" : "Alerta";
+            tipo = tipo === "Crítico" ? "Crítico" : "Alerta";
 
         }
 
         if (umidade > 55) {
             motivo.push("Umidade alta");
-            tipo = tipo === "Critico" ? "Critico" : "Alerta";
+            tipo = tipo === "Crítico" ? "Crítico" : "Alerta";
         } else if (umidade < 20) {
             motivo.push("Umidade muito baixa");
-            tipo = "Critico";
+            tipo = "Crítico";
         } else if (umidade < 40) {
             motivo.push("Umidade baixa");
-            tipo = tipo === "Critico" ? "Critico" : "Alerta";
+            tipo = tipo === "Crítico" ? "Crítico" : "Alerta";
         }
 
             const [result] = await poolBancoDados.execute(
