61c88ea28b9b07098f8ab33d4d44a04b869b29c2
rafael <rafael.vieira@sptech.school>
2025-06-10 11:45:15 -0300

    Arrumando o select de alerta

--- src/models/alertaModel.js
+++ src/models/alertaModel.js
@@ -1,15 +1,29 @@
 var database = require("../database/config");
 
-function listarAlertasAtivos() {
+function listarAlertasAtivos(ID_DATACENTER) {
     var instrucao = `
-        SELECT 
-            id,
-            fkRegistro,
-            status,
-            tipo,
-            motivo
-        FROM alerta
-        WHERE status = 'ativo';
+    SELECT 
+        a.id AS id_alerta,
+        a.status,
+        a.tipo,
+        a.motivo,
+        a.data_resolucao,
+        r.id AS id_registro,
+        r.temperatura,
+        r.umidade,
+        r.dataRegistro,
+        se.fkSala as id_sensor,
+        se.nome AS nome_sensor,
+        s.id as id_sala,
+        s.nome AS nome_sala,
+        d.id as id_datacenter,
+        d.nome AS nome_datacenter
+    FROM alerta a
+    INNER JOIN registro r ON a.fkRegistro = r.id
+    INNER JOIN sensor se ON r.fkSensor = se.fkSala
+    INNER JOIN sala s ON se.fkSala = s.id
+    INNER JOIN datacenter d ON s.fkDatacenter = d.id
+    WHERE a.status = 'ativo' and d.id = ${ID_DATACENTER};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
